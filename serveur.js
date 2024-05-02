09eb3ab3b076677ace5b06a0c9ea4d2a318707cb
flojucv <66565404+flojucv@users.noreply.github.com>
2024-05-02 09:55:41 +0200

    update

--- serveur.js
+++ serveur.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express()
+const app = express();
 const port = 3000
 const version = "v1"
 const router = require('./routes/routes');
@@ -8,6 +8,18 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const options = require('./swagger.json');
 const specs = swaggerJsdoc(options);
 
+function myCors(req, res, nxt) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent');
+  if(req.method === 'OPTIONS') {
+      res.sendStatus(204);
+  }
+  else {
+      nxt();
+  }
+}
+app.use(myCors);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(`/api/${version}`, router);
